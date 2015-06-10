1f1309f4d64fe3aa8f7cd97d3e3960c53677e4a9
Alex Weber <alexweber15@gmail.com>
2015-06-10 12:39:36 -0300

    improve "watch" task

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
+var path = require('path');
+var util = require('gulp-util');
 
 // Watch for changes.
 gulp.task('watch', function(){
-  gulp.watch([paths.html], ['html']);
-  gulp.watch([paths.js], ['js']);
-  gulp.watch([paths.sass], ['sass']);
+  gulp.watch([paths.html], ['html']).on('change', logChanges);
+  gulp.watch([paths.js], ['js']).on('change', logChanges);
+  gulp.watch([paths.sass], ['sass']).on('change', logChanges);
 });
+
+function logChanges(event) {
+  util.log(
+    util.colors.green('File ' + event.type + ': ') +
+    util.colors.magenta(path.basename(event.path))
+  );
+}
