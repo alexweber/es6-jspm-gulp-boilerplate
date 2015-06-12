090186e933e8004acb54ea6c5cb505e23ba95b9c
Alex Weber <alexweber15@gmail.com>
2015-06-12 11:51:36 -0300

    implement sass linting

--- gulp/lint.js
+++ gulp/lint.js
@@ -4,13 +4,20 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var scsslint = require('gulp-scss-lint');
 
+// Lint JS.
 gulp.task('lintjs', function () {
   return gulp.src(paths.js)
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
+// Lint SASS.
 gulp.task('lintsass', function () {
-
+  return gulp.src(paths.sass)
+    .pipe(scsslint());
 });
+
+// Lint all the things!
+gulp.task('lint', ['lintjs', 'lintsass']);
