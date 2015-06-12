c5c7902a6d028c7a6601661ea15eb95aa35c58f4
Alex Weber <alexweber15@gmail.com>
2015-06-12 10:46:34 -0300

    pass options to sass task

--- gulp/sass.js
+++ gulp/sass.js
@@ -7,12 +7,16 @@ var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
 
 // Compile SASS with sourcemaps + livereload.
 gulp.task('sass', function() {
   gulp.src(paths.sass)
     .pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(concat('app.css'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./src/css'))
