cd22c7bf1dfa704a202b5174fbbd99f3db89a61a
Alex Weber <alexweber15@gmail.com>
2015-06-12 15:40:12 -0300

    it works!!!!!

--- karma.conf.js
+++ karma.conf.js
@@ -30,9 +30,9 @@ module.exports = function(config) {
       packages: 'src/lib'
     },
 
-    // proxies: {
-      // '/base/lib/': '/base/src/lib/'
-    // },
+    proxies: {
+      '/base/lib/': '/base/src/lib/'
+    },
 
     // list of files to exclude
     exclude: [
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['progress', 'mocha'],
 
 
     // web server port
