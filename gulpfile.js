ada8029cddda8e0e2d56234990296dcfec92cc8c
Alex Weber <alexweber15@gmail.com>
2015-06-13 14:58:23 -0300

    cleanup main gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
+'use strict';
+
+/*
+ * gulpfile.js
+ * ===========
+ * Rather than manage one giant configuration file responsible
+ * for creating multiple tasks, each task has been broken out into
+ * its own file in the 'gulp' folder. Any files in that directory get
+ *  automatically required below.
+ *
+ * To add a new task, simply add a new task file in that directory.
+ */
+
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 
-// Paths.
+// Specify file paths for tasks.
 global.paths = {
   'html': './src/*.html',
   'js': './src/js/**/*.js',
   'sass': './src/scss/**/*.scss',
-  'img': './src/img/*'
+  'img': './src/img/*',
+  'dist': './dist'
 };
 
-// Require all tasks in gulp/tasks.
+// Require all tasks in the 'gulp' folder.
 requireDir('./gulp', { recurse: false });
 
+// Default task; start local server & watch for changes.
 gulp.task('default', ['connect', 'watch']);
